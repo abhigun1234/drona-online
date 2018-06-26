3e62e0e8d6db2d34be50b0efb7ced727ef19188d
Gungun <gungun@Gunguns-MacBook-Pro.local>
2018-06-26 22:08:14 +0530

    diployin on hiroku

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express=require('express');
 const app=express()
 const path=require('path')
-app.use(express.static(__dirname + '/dist'))
+app.use(express.static('./dist/dronaonline'))
 app.listen(process.env.PORT || 8080);
 
 //pathLocationStrategy
 app.get('/*',function(req,res)
 {
 
-    res.sendFile(path.join(__dirname + '/dist/index.html'))
+    res.sendFile(path.join(__dirname + '/dist/dronaonline/index.html'))
 })
 console.log('hello')

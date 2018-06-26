7416d9a3a42d550cb30a47590fe9c2876760e87f
Gungun <gungun@Gunguns-MacBook-Pro.local>
2018-06-26 19:23:04 +0530

    diployin on hiroku

--- server.js
+++ server.js
@@ -5,14 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static( './dist/dronaonline'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/dronaonline/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080,()=>{
-    console.log('Server  Started')
-});
+app.listen(process.env.PORT || 8080);
